ef43bff13721b29a552b5cab71757c5ed32d8352
Phong <p7157568@gmail.com>
2016-08-22 21:34:02 +0700

    feat - make branch dissapear after 0.5s

--- app/src/main/java/com/example/saulmm/splashes/OnBoardingWithSimpleBackground.java
+++ app/src/main/java/com/example/saulmm/splashes/OnBoardingWithSimpleBackground.java
@@ -2,7 +2,6 @@ package com.example.saulmm.splashes;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -14,10 +13,12 @@ public class OnBoardingWithSimpleBackground extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // TODO: 8/22/16 set the branch appear first
         // Fake a long startup time
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                // TODO: 8/22/16 after 0.5s make the windows dissappear(and branch icon)
                 setTheme(R.style.AppTheme);
                 setContentView(R.layout.activity_onboarding_simple);
             }
